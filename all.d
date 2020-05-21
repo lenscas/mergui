e5b6baf33c3d4cc1faf0bf5bbff036c7f40aebdd
lenscas <lenscas@users.noreply.github.com>
2020-05-21 15:12:35 +0000

    deploy: a0b89d4f4d3539c9b6c55951c481e97d88fa76d2

---
+++ all.d
@@ -0,0 +1,5 @@
+/home/runner/work/mergui/mergui/target/wasm32-unknown-unknown/release/examples/all.wasm: examples/all.rs
+
+/home/runner/work/mergui/mergui/target/wasm32-unknown-unknown/release/examples/all.d: examples/all.rs
+
+examples/all.rs:
--- src/core/mod.rs
+++
@@ -1,8 +0,0 @@
-mod image;
-pub mod image_button;
-mod text;
-pub mod text_button;
-pub use image::Image;
-pub use image_button::ImageButtonConfig;
-pub use text::Text;
-pub use text_button::TextButtonConfig;
