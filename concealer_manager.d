4c7686a7e963681db9e3c9b1797d61e3176806cf
lenscas <lenscas@users.noreply.github.com>
2020-06-06 15:55:19 +0000

    deploy: 57557b62697ae921bc755f6219b142158004a46b

---
+++ concealer_manager.d
@@ -0,0 +1,5 @@
+/home/runner/work/mergui/mergui/target/wasm32-unknown-unknown/release/examples/concealer_manager.wasm: examples/concealer_manager.rs
+
+/home/runner/work/mergui/mergui/target/wasm32-unknown-unknown/release/examples/concealer_manager.d: examples/concealer_manager.rs
+
+examples/concealer_manager.rs:
